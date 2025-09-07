2eb4e0636b2a645ab658ce289f37bf61d4068d5a
abe1abera <abelaberab@gmail.com>
2025-09-07 18:48:27 +0300

    commiting

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,10 @@
 # views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+
+
+
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.detail import DetailView
 from .models import Book
@@ -14,3 +20,24 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
+
+# User Registration View
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")  # after register, go to login
+    else:
+        form = UserCreationForm()
+    return render(request, "relationship_app/register.html", {"form": form})
+
+
+# Login View (built-in)
+class CustomLoginView(LoginView):
+    template_name = "relationship_app/login.html"
+
+
+# Logout View (built-in)
+class CustomLogoutView(LogoutView):
+    template_name = "relationship_app/logout.html"
