0e88ec5a6bc37e2d74a500baae2156c564a6a5ba
abe1abera <abelaberab@gmail.com>
2025-09-07 18:57:50 +0300

    Fix: combined task 1 urls and authentication urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import list_books     # checker requires this exact import
-from .views import LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import list_books, LibraryDetailView, register
 
 urlpatterns = [
+    # Task 1 requirements
     path("books/", list_books, name="list_books"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
+
+    # Authentication task requirements
+    path("register/", register, name="register"),
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
 ]
