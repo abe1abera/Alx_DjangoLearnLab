cd491bf7c472b1ad6cf04c44906ac79cf10c904e
abe1abera <abelaberab@gmail.com>
2025-09-12 19:10:30 +0300

    Add advanced_features_and_security project with custom user model (bookshelf.CustomUser)

---
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -0,0 +1,41 @@
+# models.py
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom manager for CustomUser.
+    """
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        if not username:
+            raise ValueError(_("The Username must be set"))
+        email = self.normalize_email(email) if email else None
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+
+        return self.create_user(username, email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    """
+    Custom user model that extends AbstractUser.
+    """
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to="profile_photos/", null=True, blank=True)
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
