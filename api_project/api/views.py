5af225dc058cea50bbb6bcaceccf2d5232c7dce2
abe1abera <abelaberab@gmail.com>
2025-09-21 02:41:29 +0300

    h

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,3 +5,15 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A simple ViewSet for viewing, creating, updating, and deleting books.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
