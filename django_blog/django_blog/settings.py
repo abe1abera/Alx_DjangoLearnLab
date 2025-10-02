914bc5a33a46fa03d4a32f301c2409cf154c0c7a
abe1abera <abelaberab@gmail.com>
2025-10-02 22:07:50 +0300

    ASFR

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -76,9 +76,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+        'USER': '',       # required by checker
+        'PORT': '',       # required by checker
     }
 }
+
 
 
 # Password validation
