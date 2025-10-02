01ec762a610d5227d0ac970534b9f4b587046df3
abe1abera <abelaberab@gmail.com>
2025-10-02 22:42:35 +0300

    Asd

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,72 +1,30 @@
 from django.urls import path
-from . import views
-
-app_name = 'blog'
-
-urlpatterns = [
-    path('', views.index, name='index'),
-]
-
-from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+)
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.index, name='index'),  # your homepage view
+    # Homepage
+    path('', views.index, name='index'),
+
+    # Authentication
     path('register/', views.register_view, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/', views.profile_view, name='profile'),
-]
 
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls', namespace='blog')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-from django.urls import path
-from .views import (
-    PostListView,
-    PostDetailView,
-    PostCreateView,
-    PostUpdateView,
-    PostDeleteView,
-)
-
-urlpatterns = [
-    path("posts/", PostListView.as_view(), name="post-list"),
-    path("posts/new/", PostCreateView.as_view(), name="post-create"),
-    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
-    path("posts/<int:pk>/edit/", PostUpdateView.as_view(), name="post-update"),
-    path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
-]
-
-
-from django.urls import path
-from .views import (
-    PostListView,
-    PostDetailView,
-    PostCreateView,
-    PostUpdateView,
-    PostDeleteView,
-)
-
-urlpatterns = [
+    # Blog Posts (CRUD)
     path("posts/", PostListView.as_view(), name="post-list"),
     path("posts/new/", PostCreateView.as_view(), name="post-create"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path("posts/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
     path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
 ]
-
