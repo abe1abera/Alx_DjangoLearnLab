52209e51a3db26ca86efe0607763d20d7749923c
abe1abera <abelaberab@gmail.com>
2025-10-02 22:05:23 +0300

    Add Post model with User ForeignKey

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,16 +1,11 @@
-# blog/models.py
 from django.db import models
-from django.conf import settings
+from django.contrib.auth.models import User   # <-- required import
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name='posts'
-    )
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
 
     def __str__(self):
-        return f"{self.title} by {self.author}"
+        return self.title
