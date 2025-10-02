aa0b5adb13806cfda17e241e3c6db45b5cbea330
abe1abera <abelaberab@gmail.com>
2025-10-02 22:35:49 +0300

    B

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -21,3 +21,13 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('bio', 'avatar')
+
+
+
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
