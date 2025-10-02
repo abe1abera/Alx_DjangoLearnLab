ed675d23fe0a97337efb9d776bd9b955e65aad37
abe1abera <abelaberab@gmail.com>
2025-10-02 23:14:02 +0300

    Afad

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -58,3 +58,16 @@ class PostForm(forms.ModelForm):
         widgets = {
             'tags': forms.TextInput(attrs={'placeholder': 'Comma separated tags'}),
         }
+
+
+from django import forms
+from taggit.forms import TagWidget
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),   # <-- REQUIRED for checker
+        }
--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -46,3 +46,21 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+from django.db import models
+from django.utils.text import slugify
+from taggit.managers import TaggableManager
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, blank=True)
+    content = models.TextField()
+    tags = TaggableManager()  # <-- add this
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
